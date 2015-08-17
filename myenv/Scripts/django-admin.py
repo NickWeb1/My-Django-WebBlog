4bea72473cd026a875911ef0eb7588a80d8fadd3
NickWeb1 <NichCason@hotmail.com>
2015-08-17 19:22:21 +0300

    My Django Web Blog, first commit

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\User\Documents\PyCharm Projects\django\myenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
